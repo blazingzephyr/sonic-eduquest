c0d490f5f030d130a81c82df3629889e6fddbe62
TenaciousKirikot <blazerzeif@gmail.com>
2022-12-25 19:57:01 +0700

    Fix spelling
    Removed the extra whitespace in PropertyDrawerUtility.cs

--- Assets/Editor/Scripts/PropertyDrawerUtility.cs
+++ Assets/Editor/Scripts/PropertyDrawerUtility.cs
@@ -15,9 +15,9 @@ namespace SonicEduquest
                 GUI.enabled = false;
 
                 string fieldName = data.Field.Name;
-                string not = data.Field.GetValue(data.Target).IsUnityNull() ? String.Empty : "not";
+                string not = data.Field.GetValue(data.Target).IsUnityNull() ? " " : " not ";
 
-                GUIContent label = new GUIContent($"{fieldName} is {not} null.", data.Property.tooltip);
+                GUIContent label = new GUIContent(fieldName + " is" + not + "null.", data.Property.tooltip);
                 EditorGUILayout.LabelField(label);
                 GUI.enabled = enabled;
             }
